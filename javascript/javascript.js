cb6218754a85ca0404b84babe6e16a3eae9b835a
sophiamuntinga <sophiamuntinga@gmail.com>
2024-05-29 17:26:05 +0200

    1e changes

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -4,10 +4,18 @@ const knopEten = document.querySelector(".eten");
 const knopSlapen = document.querySelector(".slapen");
 const knopLiefde = document.querySelector(".liefde");
 let audioEten = new Audio("./afbeeldingen-links/Fish Eat (Nr. 2  Fortnite Sound) - Sound Effect for editing.mp3")
+let naamveld = document.querySelector("#naamveld");
 
 function FishEats(){
     audioEten.play();
 }
+
+function groet(naam, typeGroet){
+    console.log("groet persoon");
+    naamveld.textContent = typeGroet + " " + naam;
+}
+
+groet("lisa", "goedemiddag");
 
 knopEten.addEventListener("click", FishEats);
  
