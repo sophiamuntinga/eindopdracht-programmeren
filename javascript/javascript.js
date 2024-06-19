bd2e87183c6ad13899ea261c14389f7ab6a65544
sophiamuntinga <sophiamuntinga@gmail.com>
2024-06-19 11:49:33 +0200

    changes met diego

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,28 +1,39 @@
-let naamInput = document.querySelector("input");
-let randomNaamKnop = document.querySelector("#nameGenerator");
+const naamInput = document.querySelector("input");
+const randomNaamKnop = document.getElementById("nameGenerator");
+const naamKiezer = document.getElementById("naamKiezer")
+const beginScherm = document.getElementById("beginScherm");
 
 const knopEten = document.querySelector("#eten");
 const knopSlapen = document.querySelector("#slapen");
 const knopLiefde = document.querySelector("#liefde");
 
-let audioEten = new Audio("./afbeeldingen-links/eating-sound.mp3");
-let audioSlapen = new Audio("./afbeeldingen-links/sleeping-sound.mp3");
-let audioLiefde = new Audio("./afbeeldingen-links/kissing-sound.mp3")
+const audioEten = new Audio("./afbeeldingen-links/eating-sound.mp3");
+const audioSlapen = new Audio("./afbeeldingen-links/sleeping-sound.mp3");
+const audioLiefde = new Audio("./afbeeldingen-links/kissing-sound.mp3");
 
-let naamveld = document.querySelector("#naamveld");
+const naamveld = document.querySelector("#naamveld");
 const standaardNamen = ["Indy", "Octo", "Janneke", "Henk", "Bo", "Tineke"];
 
-let octImage = document.querySelector("img");
+const octImage = document.querySelector("img");
 
-let hungerBar = document.querySelector("#hongerVulling");
+const hungerBar = document.querySelector("#hongerVulling");
+const slaapBar = document.querySelector("#slaapVulling");
+
+let octopusNaam;
+let naamGekozen = false;
 let widthHunger = 100;
 let widthSlaap = 100;
 let hungerID;
-let slaapBar = document.querySelector("#slaapVulling")
 
-
-
-
+function geefNaam() {
+    octopusNaam = naamInput.value;
+    if(octopusNaam == ""){
+        naamGekozen = false;
+    } else {
+        beginScherm.style.display = "none";
+        naamGekozen = true;
+    }
+}
 function generateRandomName() {
     const getal = Math.floor(Math.random() * 6);
     naamInput.value = standaardNamen[getal];
@@ -34,7 +45,7 @@ function playSound(audio){
     audio.play();
 }
 
-// Bij deze functie heeft mijn broertje me geholpen. Het zorgt ervoor dat de default afbeelding de rode-inkvis.png is en de andere afbeeldingen voor een bepaalde tijd bijven, waarna de default afbeelding weer tevoorschijn komt.
+// Bij deze functie heeft mijn broertje me geholpen. Het zorgt ervoor dat de default afbeelding de rode-inkvis.png is en de andere afbeeldingen voor een bepaalde tijd bijven, waarna de default afbeelding weer tevoorschijn komt
 function changeImage(imgLink) {
     const defaultImgLink = "afbeeldingen-links/rode-inktvis.png";
     octImage.src = imgLink;
@@ -51,12 +62,13 @@ function changeImage(imgLink) {
 
 // Hier hebben mijn broertje en Diego me mee geholpen. Het stukje ${widthHunger}% is gescherven door chatgpt. Het zorgt ervoor dat de bar van de honger elke seconde -1 (naar links gaat).
 function hungerDown() {
-    console.log(widthHunger)
-    if (widthHunger <= 0) {
-        // ONTPLOFFING
-    } else {
-        hungerBar.style.width = `${widthHunger}%`;
-        widthHunger--;
+    if(naamGekozen == true){
+        if (widthHunger <= 0) {
+            // ONTPLOFFING
+        } else if (widthSlaap >= 0){
+            hungerBar.style.width = `${widthHunger}%`;
+            widthHunger--;
+        }
     }
 }
 
@@ -70,17 +82,17 @@ function gainHunger() {
     }
 }
 
-// deze interval zorgt ervoor dat de funtie hungerDown automatisch constant wordt uitgevoerd.
-setInterval(hungerDown, 1000);
 
 // deze funtie is hetzelfde als de hungerDown funtie, maar dan voor de slaapknop.
 function slaapBarDown() {
     console.log(widthSlaap)
-    if (widthSlaap <= 0) {
-        // ONTPLOFFING
-    } else {
-        slaapBar.style.width = `${widthSlaap}%`;
-        widthSlaap--;
+    if(naamGekozen == true){
+        if (widthSlaap <= 0) {
+            // ONTPLOFFING
+        } else if (widthSlaap >= 0){
+            slaapBar.style.width = `${widthSlaap}%`;
+            widthSlaap--;
+        }
     }
 }
 
@@ -97,17 +109,15 @@ function slaapBarUp() {
 // deze interval zorgt ervoor dat de funtie slaapBarDown automatisch constant wordt uitgevoerd.
 setInterval(slaapBarDown, 1000);
 
-// function groet(naam, typeGroet){
-//     console.log("groet persoon");
-//     naamveld.textContent = typeGroet + " " + naam;
-// }
-
-// groet("lisa", "goedemiddag");
-
+// deze interval zorgt ervoor dat de funtie hungerDown automatisch constant wordt uitgevoerd.
+setInterval(hungerDown, 1000);
 
 
 // deze event listener zorgt ervoor dat de funtie generateRandomName wordt uitgevoerd
 randomNaamKnop.addEventListener("click", generateRandomName)
+
+
+naamKiezer.addEventListener("click", geefNaam)
 
 // deze eventlisteners zorgen ervoor dat de functies gainHunger en slaapBarUp worden uitgevoerd
 knopEten.addEventListener("click", gainHunger);
